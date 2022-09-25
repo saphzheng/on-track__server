a209e587d344a68815910a789f4a4e6233cd48c2
sapphira-zheng <sapphirazheng@gmail.com>
2022-09-25 17:18:33 -0400

    Add route to get exercises by search term

--- controllers/exerciseController.js
+++ controllers/exerciseController.js
@@ -15,7 +15,17 @@ const getByBodyPart = (req, res) => {
     res.json(filteredData);
 };
 
+// '/exercise/search/:q' => get list of exercises containing search word
+const getBySearch = (req, res) => {
+    const q = req.params.q.toLowerCase();
+    const exerciseData = JSON.parse(fs.readFileSync("./data/exercises.json"));
+    const filteredData = exerciseData.filter(exercise => exercise.name.includes(q) || 
+        exercise.bodyPart.includes(q) || exercise.target.includes(q) || exercise.equipment.includes(q));
+    res.json(filteredData);
+};
+
 module.exports = {
     index,
-    getByBodyPart
+    getByBodyPart,
+    getBySearch
 }
--- routes/exerciseRoute.js
+++ routes/exerciseRoute.js
@@ -8,4 +8,7 @@ router.route('/').get(exerciseController.index);
 // '/exercise/:bodyPart' routes
 router.route('/:bodyPart').get(exerciseController.getByBodyPart);
 
+// '/exercise/search/:q' routes
+router.route('/search/:q').get(exerciseController.getBySearch);
+
 module.exports = router;
